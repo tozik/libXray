260185bc5e40f41aed8f86fa156a65be82ec5e84
Aleksander <tozika@inbox.ru>
2024-03-20 17:58:39 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -2,14 +2,14 @@
 import PackageDescription
 
 let package = Package(
-  name: "XrayKit",
+  name: "libXray",
   platforms: [.iOS(.v15), .macOS(.v12)],
   products: [
-    .library(name: "LibXrayCustom", targets: ["LibXray"])
+    .library(name: "libXray", targets: ["libXray"])
   ],
   targets: [
     .binaryTarget(
-      name: "LibXray",
+      name: "libXray",
       url: "https://github.com/tozik/libXray/releases/download/1.8.9/LibXray.xcframework.zip"",
       checksum: "bbf4c31dd8d54a9888f8844c26262e46224448e804191137a4a7b06ab4be68b6"
     )
