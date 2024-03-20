767cc2079079be694b29fc56b56a8c221e60c294
Aleks Tozik <tozika@inbox.ru>
2024-03-20 17:03:32 +0000

    LibXray Release 1.8.9!!!

--- Package.swift
+++ Package.swift
@@ -2,16 +2,16 @@
 import PackageDescription
 
 let package = Package(
-  name: "libXray",
+  name: "XrayKit",
   platforms: [.iOS(.v15), .macOS(.v12)],
   products: [
-    .library(name: "libXray", targets: ["libXray"])
+    .library(name: "LibXrayCustom", targets: ["LibXray"])
   ],
   targets: [
     .binaryTarget(
-      name: "libXray",
+      name: "LibXray",
       url: "https://github.com/tozik/libXray/releases/download/1.8.9/LibXray.xcframework.zip"",
-      checksum: "bbf4c31dd8d54a9888f8844c26262e46224448e804191137a4a7b06ab4be68b6"
+      checksum: "ed34ea1f3348eb03d1239f2c7d59f2c176c2e0caa8ce0ebc87dd8c7d079072ac"
     )
   ]
 )
